aace7c4ad3a6af4291e11793429e970d7b7408ec
Santos <santosphone22@gmail.com>
2026-04-11 03:04:36 -0600

    configuracion del intermediario de webhook de discord y github

---
+++ index.ts
@@ -0,0 +1,42 @@
+import "dotenv/config";
+import express, { type Request, type Response } from "express";
+
+const app = express();
+const PORT = Number(process.env.PORT ?? 3000);
+const DISCORD_WEBHOOK = process.env.WEBHOOK_DISCORD;
+
+app.use(express.json());
+
+app.post("/github-webhook", async (req: Request, res: Response) => {
+  try {
+    const data = req.body;
+    if (!DISCORD_WEBHOOK) {
+      console.error("WEBHOOK_DISCORD no está definido");
+      return res.sendStatus(500);
+    }
+
+    const repo = data.repository?.name;
+    const user = data.pusher?.name;
+
+    const message = `🚀 Push en ${repo} por ${user}`;
+
+    await fetch(DISCORD_WEBHOOK, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        content: message
+      })
+    });
+
+    res.sendStatus(200);
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(500);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
