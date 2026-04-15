078f00cb75ac6d9aa9129f4dbeae85417c4f8d72
Santos <santosphone22@gmail.com>
2026-04-14 21:03:49 -0600

    feat(webhook): add scheduled updates for Dollar price to Discord channel

--- index.ts
+++ index.ts
@@ -37,6 +37,41 @@ app.post("/github-webhook", async (req: Request, res: Response) => {
   }
 });
 
+const DISCORD_CHANNEL_ID = process.env.MONEY_WEBHOOKS_DISCORD;
+const URL_BASE = process.env.URL_BASE;
+setInterval(async () => {
+  try {
+    const response = await fetch(`${URL_BASE}/cotizaciones/usd`);
+    const data = await response.json();
+    const price = data.compra;
+
+    if (!DISCORD_CHANNEL_ID) {
+      console.error("MONEY_WEBHOOKS_DISCORD no está definido");
+      return;
+    }
+
+    const message = `
+      💰 Precio actual del Dólar: $${price}
+      Precio de compra: $${data.compra}
+      Precio de venta: $${data.venta}
+      Precio fix: $${data.fix}
+      Fecha de actualización: ${data.fechaActualizacion}
+    `;
+
+    await fetch(DISCORD_CHANNEL_ID, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        content: message
+      })
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}, 2 * 60 * 1000);
+
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
